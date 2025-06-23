75ee0aa19c29ab89368791768170adccb4f9bd4b
ZaimaSyarifaAsshafa <138386280+ZaimaSyarifaAsshafa@users.noreply.github.com>
2025-06-23 22:03:37 +0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,7 +4,6 @@ import requests
 # Konfigurasi API
 OPENROUTER_API_KEY = st.secrets["OPENROUTER_API_KEY"]
 MODEL = "openai/gpt-3.5-turbo"
-API_URL = "https://openrouter.ai/api/v1/chat/completions"
 
 HEADERS = {
     "Authorization": f"Bearer {OPENROUTER_API_KEY}",
